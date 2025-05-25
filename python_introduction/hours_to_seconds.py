c3eeddda1b87946a85ae6183c048b39114f0bf01
KelvenFigueiredo <kelven.de.figueiredo.leopoldo@gmail.com>
2025-05-25 20:34:34 +0200

    implementing hours conversor to secounds in python

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+hours = 2 #value representing the number of hours you want to convert to seconds
+
+#Calculate the number of seconds in the given hours
+seconds = hours * 60 * 60
+
+print(f"{hours} hour(s) is {seconds} seconds.")
