1b33a1e9a2b502e18f2043805bd203febc4f96b7
KelvenFigueiredo <kelven.de.figueiredo.leopoldo@gmail.com>
2025-06-08 19:36:21 +0200

    FIX: implementing using if

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,15 @@
 #function that performs basic arithmetic operations
 def perform_operation(num1, num2, operation):
-        match operation:
-                case "add":
-                    return num1 + num2
-                case "subtract":
-                    return num1 - num2
-                case "multiply":
-                    return num1 * num2
-                case "divide":
-                    if num2 != 0:
-                        return num1 / num2
-                    else:
-                        return "Cannot divide by zero."
-                case _: 
-                    return "Invalid operator."
+        if operation == "add":
+             return num1 + num2
+        elif operation == "subtract":
+            return num1 - num2
+        elif operation =="multiply":
+            return num1 * num2
+        elif operation == "divide":
+            if num2 != 0:
+                return num1 / num2
+            else:
+                return "Cannot divide by zero."
+        else:
+            return "Invalid operator."
