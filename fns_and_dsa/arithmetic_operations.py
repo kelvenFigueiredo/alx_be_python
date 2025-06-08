5544cb8ade077e74f7ae52129a491ef8bc071162
KelvenFigueiredo <kelven.de.figueiredo.leopoldo@gmail.com>
2025-06-08 19:28:30 +0200

    FIX: solving return buggs

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,19 +2,15 @@
 def perform_operation(num1:float, num2:float, operation:str):
         match operation:
                 case "add":
-                    result = num1 + num2
-                    return result
+                    return num1 + num2
                 case "subtract":
-                    result = num1 - num2
-                    return result
+                    return num1 - num2
                 case "multiply":
-                    result = num1 * num2
-                    return result
+                    return num1 * num2
                 case "divide":
                     if num2 != 0:
-                        result = num1 / num2
-                        return result
+                        return num1 / num2
                     else:
                         return "Cannot divide by zero."
                 case _: 
-                    print("Invalid operator.")
+                    return "Invalid operator."
